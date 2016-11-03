d3d2788db7e577f9fdcb72dccbf14a10eaaec498
Rogério da Rocha Rodrigues <rogeriorodrigues@outlook.com>
2016-11-03 20:55:41 -0200

    Commmit inicial

---
+++ lgroup/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Acr.UserDialogs;
+
+namespace lgroup
+{
+	public partial class MainPage : ContentPage
+	{
+		
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void AddClick(object sender, System.EventArgs e)
+		{
+			/// <summary>
+			/// Exemplo de como navegar com uma page: PushAsync ,
+			///  PushModalAsync, PopModalAsync, PopAsync
+			/// </summary>
+			//Navigation.PushModalAsync(new PageCriada());
+		}
+	}
+}
---
+++ lgroup/lgroupPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace lgroup
+{
+	public partial class lgroupPage : ContentPage
+	{
+		public lgroupPage()
+		{
+			InitializeComponent();
+		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			btnAlerta.Text = "Cliquei";
+
+			if(Device.OS == TargetPlatform.iOS)
+				await DisplayAlert("Mensagem", "iOS " + txtNome.Text, "OK", "Cancel");
+			else if (Device.OS == TargetPlatform.Android)
+				await DisplayAlert("Mensagem", "Android " + txtNome.Text, "OK", "Cancel");
+		}
+
+		void fecharClick(object sender, System.EventArgs e)
+		{
+			Navigation.PopModalAsync();
+		}
+	}
+}
